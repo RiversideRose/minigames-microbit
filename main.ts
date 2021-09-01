7fa5ed90b0e3d4c3f30534467a609840c9f80abe
RiversideRose <66091556+RiversideRose@users.noreply.github.com>
2021-09-01 09:11:14 -0700

    `Added radio support for multiple mircobits and fixed the explode animation.

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-let rock = 0
-let paper = 0
-let scissors = 0
-let amogla = 0
 input.onButtonPressed(Button.A, function () {
     rock = 0
     paper = 0
@@ -9,6 +5,8 @@ input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 2; index++) {
         amogla = randint(0, 2)
         if (amogla == 0) {
+            radio.sendString("Paper")
+            radio.sendValue("Paper", 1)
             basic.showLeds(`
                 . . # # #
                 . . . . .
@@ -86,8 +84,10 @@ input.onButtonPressed(Button.A, function () {
                 . . . . .
                 . . . . .
                 `)
-            paper = 1
         } else if (amogla == 1) {
+            radio.sendString("Rock")
+            radio.sendValue("Rock", 1)
+            rock = 1
             basic.showLeds(`
                 . . . . .
                 . . . . .
@@ -235,8 +235,9 @@ input.onButtonPressed(Button.A, function () {
                 . . . . .
                 . . . . .
                 `)
-            rock = 1
         } else {
+            radio.sendString("Scissors")
+            scissors = 1
             basic.showLeds(`
                 # # . . #
                 # # . # .
@@ -272,9 +273,10 @@ input.onButtonPressed(Button.A, function () {
                 # # . # .
                 # # . . #
                 `)
-            scissors = 1
         }
     }
+    radio.sendValue("splode", 1)
+    radio.sendString("\"boom\"")
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -417,7 +419,7 @@ input.onButtonPressed(Button.A, function () {
         `)
     basic.showLeds(`
         . . # . .
-        . # # # #
+        . # # # .
         # # # # #
         . # # # .
         . . # . .
@@ -444,6 +446,7 @@ input.onButtonPressed(Button.A, function () {
         . . . . .
         `)
     if (paper == 1 && rock == 1) {
+        radio.sendString("pwin")
         led.plotBrightness(0, 0, 100)
         led.plotBrightness(0, 1, 100)
         led.plotBrightness(1, 1, 100)
@@ -461,11 +464,14 @@ input.onButtonPressed(Button.A, function () {
             # # # # #
             `)
         basic.pause(1000)
+        radio.sendString("Paper Wins!")
         basic.showString("Paper Wins!")
     } else {
         basic.clearScreen()
     }
     if (paper == 1 && scissors == 1) {
+        radio.sendValue("swin", 1)
+        radio.sendString("swin")
         led.plotBrightness(0, 0, 100)
         led.plotBrightness(0, 1, 100)
         led.plotBrightness(1, 1, 100)
@@ -483,11 +489,14 @@ input.onButtonPressed(Button.A, function () {
             . . # . .
             `)
         basic.pause(1000)
+        radio.sendString("Scissors Win!")
         basic.showString("Scissors Win!")
     } else {
         basic.clearScreen()
     }
     if (paper == 1 && paper == 1) {
+        radio.sendString("tie")
+        radio.sendValue("tie", 1)
         led.plotBrightness(0, 0, 100)
         led.plotBrightness(0, 1, 100)
         led.plotBrightness(1, 1, 100)
@@ -505,11 +514,13 @@ input.onButtonPressed(Button.A, function () {
             . # . # .
             `)
         basic.pause(1000)
+        radio.sendString("Tie!")
         basic.showString("Tie!")
     } else {
         basic.clearScreen()
     }
     if (rock == 1 && scissors == 1) {
+        radio.sendString("rwin")
         led.plotBrightness(0, 0, 100)
         led.plotBrightness(0, 1, 100)
         led.plotBrightness(1, 1, 100)
@@ -519,6 +530,511 @@ input.onButtonPressed(Button.A, function () {
         led.plotBrightness(4, 1, 100)
         led.plotBrightness(5, 0, 100)
         led.plotBrightness(5, 1, 100)
+        basic.showLeds(`
+            # . # . #
+            # # # # #
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.pause(1000)
+        radio.sendString("Rock Wins!")
+        basic.showString("Rock Wins!")
+    } else {
+        basic.clearScreen()
+    }
+    if (rock == 1 && rock == 1) {
+        radio.sendString("tie")
+        radio.sendValue("tie", 1)
+        led.plotBrightness(0, 0, 100)
+        led.plotBrightness(0, 1, 100)
+        led.plotBrightness(1, 1, 100)
+        led.plotBrightness(2, 1, 100)
+        led.plotBrightness(3, 1, 100)
+        led.plotBrightness(3, 0, 100)
+        led.plotBrightness(4, 1, 100)
+        led.plotBrightness(5, 0, 100)
+        led.plotBrightness(5, 1, 100)
+        basic.showLeds(`
+            # . # . #
+            # # # # #
+            . # . # .
+            . . # . .
+            . # . # .
+            `)
+        basic.pause(1000)
+        radio.sendString("Tie!")
+        basic.showString("Tie!")
+    } else {
+        basic.clearScreen()
+    }
+    if (scissors == 1 && scissors == 1) {
+        radio.sendString("tie")
+        led.plotBrightness(0, 0, 100)
+        radio.sendValue("tie", 1)
+        led.plotBrightness(0, 1, 100)
+        led.plotBrightness(1, 1, 100)
+        led.plotBrightness(2, 1, 100)
+        led.plotBrightness(3, 1, 100)
+        led.plotBrightness(3, 0, 100)
+        led.plotBrightness(4, 1, 100)
+        led.plotBrightness(5, 0, 100)
+        led.plotBrightness(5, 1, 100)
+        basic.showLeds(`
+            # . # . #
+            # # # # #
+            . # . # .
+            . . # . .
+            . # . # .
+            `)
+        basic.pause(1000)
+        radio.sendString("Tie!")
+        basic.showString("Tie!")
+    } else {
+        basic.clearScreen()
+    }
+    basic.clearScreen()
+    radio.sendString("Game Over!")
+    basic.showString("Game Over!")
+})
+radio.onReceivedString(function (receivedString) {
+    let pwin = 0
+    if (receivedString == "Paper") {
+        basic.showLeds(`
+            . . # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # # #
+            . . # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # # . .
+            . . # # #
+            . . # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # # . .
+            # # # # #
+            . . # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # . . .
+            # # # # #
+            . . # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            # # # # #
+            . # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (receivedString == "Rock") {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # . # .
+            . . . . #
+            # . # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . #
+            # . . . .
+            . . # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . # . .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . #
+            . # . . .
+            . . # . .
+            . # # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            # . . . .
+            . . . # .
+            . . . . .
+            . # # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . # .
+            . . . . .
+            # . . . .
+            . . # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            . . . # .
+            . . . . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            # . . . .
+            . . . # .
+            . # . . .
+            . . . . .
+            . . # # .
+            `)
+        basic.showLeds(`
+            . . . # .
+            . # . . .
+            . . . . #
+            . . . . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . . .
+            . . . . #
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . #
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        if (receivedString == "Scissors") {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+            basic.showLeds(`
+                # # . . .
+                # # . # #
+                . . # . .
+                # # . # #
+                # # . . .
+                `)
+            basic.showLeds(`
+                # # . . .
+                # # . . .
+                . . # # #
+                # # . . .
+                # # . . .
+                `)
+            basic.showLeds(`
+                # # . . .
+                # # . # #
+                . . # . .
+                # # . # #
+                # # . . .
+                `)
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        }
+    }
+    if (receivedString == "\"boom\"") {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . # .
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # . . .
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . # .
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . # # .
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . # # . .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . . # . .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . . # . .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . . # . .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (receivedString == "rwin") {
         basic.showLeds(`
             # . # . #
             # # # # #
@@ -528,19 +1044,28 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.pause(1000)
         basic.showString("Rock Wins!")
+    } else if (pwin == 1) {
+        basic.showLeds(`
+            # . # . #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(1000)
+        basic.showString("Paper Wins!")
     } else {
-        basic.clearScreen()
+        if (receivedString == "swin") {
+            basic.showLeds(`
+                # . # . #
+                # # # # #
+                # . . . #
+                . # . # .
+                . . # . .
+                `)
+        }
     }
-    if (rock == 1 && rock == 1) {
-        led.plotBrightness(0, 0, 100)
-        led.plotBrightness(0, 1, 100)
-        led.plotBrightness(1, 1, 100)
-        led.plotBrightness(2, 1, 100)
-        led.plotBrightness(3, 1, 100)
-        led.plotBrightness(3, 0, 100)
-        led.plotBrightness(4, 1, 100)
-        led.plotBrightness(5, 0, 100)
-        led.plotBrightness(5, 1, 100)
+    if (receivedString == "tie") {
         basic.showLeds(`
             # . # . #
             # # # # #
@@ -550,34 +1075,13 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.pause(1000)
         basic.showString("Tie!")
-    } else {
-        basic.clearScreen()
     }
-    if (scissors == 1 && scissors == 1) {
-        led.plotBrightness(0, 0, 100)
-        led.plotBrightness(0, 1, 100)
-        led.plotBrightness(1, 1, 100)
-        led.plotBrightness(2, 1, 100)
-        led.plotBrightness(3, 1, 100)
-        led.plotBrightness(3, 0, 100)
-        led.plotBrightness(4, 1, 100)
-        led.plotBrightness(5, 0, 100)
-        led.plotBrightness(5, 1, 100)
-        basic.showLeds(`
-            # . # . #
-            # # # # #
-            . # . # .
-            . . # . .
-            . # . # .
-            `)
-        basic.pause(1000)
-        basic.showString("Tie!")
-    } else {
-        basic.clearScreen()
+    if (receivedString == "Game Over!") {
+        basic.showString("Game Over!")
     }
-    basic.clearScreen()
-    basic.showString("Game Over!")
 })
-input.onButtonPressed(Button.B, function () {
-	
-})
+let amogla = 0
+let scissors = 0
+let paper = 0
+let rock = 0
+radio.setGroup(1)
